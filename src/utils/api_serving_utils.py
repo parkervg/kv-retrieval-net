15e45397345cdce5c5c7f9fff7e6448565bdfe68
parkervg <parkervg5@gmail.com>
2022-05-09 23:43:31 -0400

    nltk punkt

--- src/utils/api_serving_utils.py
+++ src/utils/api_serving_utils.py
@@ -168,6 +168,7 @@ def get_prediction_json(model: "KVNetwork", text: str, item: Dict, dataset):
     print(toks[::-1])
     item["input"] = featurize(toks, dataset.tok2id)  # (1, max_seq_len)
     with torch.no_grad():
+        model.eval()
         outputs = model(item=item, teacher_forcing_ratio=0.0, sos_token_id=sos_token_id)
     pred_ids = outputs.argmax(1).squeeze()
     raw_output = utils.ids_to_text(
